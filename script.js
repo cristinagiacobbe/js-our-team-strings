e3b9c4e07c790523f4e34b5916b351d69eb4a3f1
cristinagiacobbe <crigiak@gmail.com>
2023-12-06 22:04:17 +0100

    Add javascript tools

---
+++ script.js
@@ -0,0 +1,19 @@
+//creare un array di stringhe con i nomi dei membri del team.
+//MILESTONE 0:
+//Creare l’array di stringhe contenente i nomi dei membri del team.
+//MILESTONE 1:
+//Stampare le informazioni su DOM come card.
+//MILESTONE 3:
+//Aggiungere un evento click sulla card che aggiunge/rimuove una classe per evidenziare un componente del team.
+//BONUS 1:
+//In generale curare la parte di UI e organizzare i singoli membri in card/schede.
+//BONUS 2:
+//Inserire un form per l’aggiunta di un elemento alla lista.
+
+//TOOLS
+//Strings corresponding to team member names
+//Array
+//card (class)
+//document.innerText
+//click event
+//push/remove
